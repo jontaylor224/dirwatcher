78abd00bc188fe93dc105315c9fadf83bf4f4ad5
Jon Taylor <jontaylor224@gmail.com>
2019-07-19 10:41:37 -0500

    changed params and added docstrings

--- dirwatcher.py
+++ dirwatcher.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python2
+"""
+Dirwatcher exercise
+Demonstrate long-running applications by monitoring text files in a directory
+and scanning for a specified search text.
+Users can use the command line options to specify the directory to monitor,
+the extension type of files to monitor, the time interval between each scan,
+and the text for which to scan('magic text').
+"""
 import time
 import os
 import argparse
@@ -19,37 +28,41 @@ signames = dict((k, v) for v, k in reversed(sorted(signal.__dict__.items()))
                 if v.startswith('SIG') and not v.startswith('SIG_'))
 
 
-def scan_single_file(filename, start_line, search_term):
-    '''Scan file for search_term, starting at start_line'''
+def scan_single_file(filename, start_line, magic):
+    '''Scan file for magic, starting at start_line'''
     line_num = 0
     with open(filename) as f:
         for line_num, line in enumerate(f):
             if line_num >= start_line:
-                if search_term in line:
+                if magic in line:
                     logger.info('File={}: found text={} on line={}'
-                                .format(filename, search_term, line_num + 1))
+                                .format(filename, magic, line_num + 1))
     return line_num + 1
 
 
-def watch_directory(path, ext, search_term):
-    '''Monitors the path directory for files with extension type ext containing search_term'''
+def watch_directory(path, ext, magic):
+    '''Monitors the path directory for files with extension type ext
+    containing magic text'''
     dir_files = os.listdir(path)
+
     # Check for new files
     for file in dir_files:
         if file.endswith(ext):
             if file not in watched_files:
                 logger.info('Watching new file={}'.format(file))
                 watched_files[file] = 1
+
     # Stop watching deleted files
     for file in watched_files.keys():
         if file not in dir_files:
             logger.info('Removed file={}'.format(file))
             watched_files.pop(file)
+
     # Scan watched files
     for f in watched_files:
         file_path = os.path.join(path, f)
         start_line = watched_files[f]
-        watched_files[f] = scan_single_file(file_path, start_line, search_term)
+        watched_files[f] = scan_single_file(file_path, start_line, magic)
 
 
 def create_parser():
@@ -59,19 +72,19 @@ def create_parser():
     )
     parser.add_argument('-e', '--ext', type=str, default='.txt',
                         help='Text file extension to monitor. eg. .txt, .log')
-    parser.add_argument('-i', '--interval', type=float, default=1.0,
+    parser.add_argument('-i', '--int', type=float, default=1.0,
                         help='Number of seconds of polling interval.')
     parser.add_argument('path', help='Directory to monitor.')
     parser.add_argument(
-        'search_term', help='Text string the program is seeking.')
+        'magic', help='Text string the program is seeking.')
     return parser
 
 
 def signal_handler(sig_num, frame):
     """
-   This is a handler for SIGTERM and SIGINT. 
-   Other signals can be mapped here as well (SIGHUP?)
-   Basically it just sets a global flag, and main() will exit its loop if the signal is trapped.
+   Handler for SIGTERM and SIGINT.
+   Sets a global flag which will cause main() to exit its loop if either
+   signal is trapped.
    :param sig_num: The integer signal number that was trapped from the OS.
    :param frame: Not used
    :return None
@@ -84,8 +97,10 @@ def signal_handler(sig_num, frame):
 
 def main():
     # Set up logger to print to console
+    log_format = ('%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s'
+                  '%(message)s')
     logging.basicConfig(
-        format='%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s %(message)s',
+        format=log_format,
         datefmt='%Y-%m-%d %H:%M:%S'
     )
     logger.setLevel(logging.DEBUG)
@@ -112,18 +127,18 @@ def main():
 
     logger.info(
         'Watching dir={} for files with extension={} containing text={}'
-        .format(args.path, args.ext, args.search_term))
+        .format(args.path, args.ext, args.magic))
     while not exit_flag:
         try:
-            logger.debug('looping...')
-            watch_directory(args.path, args.ext, args.search_term)
+            # logger.debug('looping...')
+            watch_directory(args.path, args.ext, args.magic)
         except OSError as e:
             logger.error("ERROR: {}".format(e))
         except Exception as e:
             logger.error("UNHANDLED EXCEPTION: {}".format(e))
             time.sleep(4)
 
-        time.sleep(args.interval)
+        time.sleep(args.int)
 
     # Setup Shutdown Banner
     uptime = dt.now() - app_start_time
@@ -131,7 +146,7 @@ def main():
         '\n'
         '********************************************************\n'
         '   Shutting down {0}\n'
-        '   Stopped on {1}\n'
+        '   Uptime was {1}\n'
         '********************************************************\n'
         .format(__file__, str(uptime))
     )
